bf4b786c6e2f504c9c3a7615d393745ba284f8cd
Jeevan <jeevanauji1431@gmail.com>
2025-12-17 18:05:42 +0530

    todays 17/12

--- src/app/budget-planner/income/income.component.ts
+++ src/app/budget-planner/income/income.component.ts
@@ -1,11 +1,27 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-income',
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './income.component.html',
   styleUrl: './income.component.scss'
 })
 export class IncomeComponent {
+
+  incomeForm: any;
+  constructor(public fb: FormBuilder) { }
+  ngOnInit(): void {
+    this.incomeForm = this.fb.group({
+      month: ['', Validators.required],
+      source: ['', Validators.required],
+      amount: ['', Validators.required],
+      investment: ['', Validators.required],
+    })
+  }
+  onSubmit() {
+
+  }
 
 }
